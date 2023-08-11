25fdd0eef88858b16fbb79992dd57b5d40123478
hcy <825532446@qq.com>
2023-08-12 00:27:31 +0800

    指针进阶

--- point_improve/point_improve/point.c
+++ point_improve/point_improve/point.c
@@ -85,22 +85,95 @@
 //}
 
 
-int main()
-{
-	int arr1[] = { 1,2,3,4,5 };
-	int arr2[] = { 2,3,4,5,6};
-	int arr3[] = { 3,4,5,6,7 };
-	int *parr[] = {arr1,arr2,arr3};
-	int i = 0;
-	for (i = 0; i < 3; i++)
-	{
-		int j = 0;
-		for (j = 0; j < 5; j++)
-		{
+//int main()
+//{
+//	int arr1[] = { 1,2,3,4,5 };
+//	int arr2[] = { 2,3,4,5,6};
+//	int arr3[] = { 3,4,5,6,7 };
+//	int *parr[] = {arr1,arr2,arr3};
+//	int i = 0;
+//	for (i = 0; i < 3; i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j < 5; j++)
+//		{
+//
+//			printf("%d ", *(parr[i] + j));
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
 
-			printf("%d ", *(parr[i] + j));
-		}
-		printf("\n");
-	}
-	return 0;
-}
+
+//int main()
+//{
+//
+//	int* p =NULL;
+//	char* pc = NULL;
+//	//数组指针-指向数组的指针-存放数组的指针
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int(*p)[10] = &arr;//上面的p就是数组指针
+//
+//}
+/*int main()
+{
+
+	char* arr[5];
+	char (*pa)[5] = &arr;
+
+	int arr
+}*/
+
+// int main()
+//{
+//
+//
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int(*pa)[10] = &arr;
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", (*pa)[i]);
+//	}
+//	return 0;
+//}
+
+//void print1(int arr[3][5], int x, int y)
+//{
+//	int i = 0;
+//	int j = 0;
+//	for (i = 0; i < x; i++)
+//	{
+//		for (j = 0; j < y; j++)
+//		{
+//			printf("%d ", arr[i][j]);
+//		}
+//		printf("\n");
+//	}
+//}
+//
+//
+//void print2(int(*p)[5], int x, int y)
+//{
+//	int i = 0;
+//	for (i = 0; i < x; i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j < y; j++)
+//		{
+//			//printf("%d ", *(*(p + i) + j));
+//			printf("%d ", (*(p + i))[j]);
+//		}
+//		printf("\n");
+//	}
+//}
+//
+//int main()
+//{
+//	int arr[3][5] = { {1,2,3,4,5},{1,2,3,4,5},{1,2,3,4,5} };
+//	print1(arr, 3, 5);
+//	print2(arr, 3, 5);
+//
+//
+//}
