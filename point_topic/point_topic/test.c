c7e029d3a50857a1d8d3cc91b1aeb15f26b9a556
hcy <825532446@qq.com>
2023-08-29 22:30:01 +0800

    ‘指针面试题’

--- point_topic/point_topic/test.c
+++ point_topic/point_topic/test.c
@@ -154,47 +154,168 @@
 //	return 0;
 //}
 
-int main()
+//int main()
+//{
+//	int line = 0;
+//	scanf("%d", &line);
+//	//打印上半部分
+//	int i = 0;
+//	for (i = 0; i < line; i++)
+//	{
+//		int j = 0;
+//		//打印空格
+//		for (j = 0; j < line - 1 - i; j++)
+//		{
+//			printf(" ");
+//		}
+//		//打印*
+//		for (j = 0; j < 2*i+1; j++)
+//		{
+//			printf("*");
+//		}
+//		printf("\n");
+//
+//		
+//	}
+//	//打印下半部分
+//	for (i = 0; i < line; i++)
+//	{
+//		//打印空格
+//		int j = 0;
+//		for (j = 0; j <=i; j++)
+//		{
+//			printf(" ");
+//		}
+//		//打印*
+//		for (j = 0; j <2*(line-1-i)-1 ; j++)
+//		{
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//
+//
+//
+//
+// }
+
+////暴力求解法
+//#include<assert.h>
+//void left_move(char *arr,int k)
+//{
+//	int i = 0;
+//	assert(arr);
+//	int len = strlen(arr);
+//	for (i = 0; i < k; i++)
+//	{
+//		char tmp = *arr;
+//		int j = 0;
+//		for (j = 0; j <len-1; j++)
+//		{
+//			*(arr + j) = *(arr + j + 1);
+//
+//		}
+//		*(arr + len - 1) = tmp;
+//
+//	}
+//}
+//int main()
+//{
+//	char arr[] = "abcdef";
+//	left_move(arr, 2);
+//	printf("%s\n",arr);
+//		return 0;
+//}
+//
+////三步翻转法
+////abcdef
+////ba fedc
+////cdefab
+//#include<assert.h>
+////逆序字符串函数
+//void reverse(char* left, char* right)
+//{
+//	assert(left);
+//	assert(right);
+//	while (left<right)
+//	{
+//		char tmp = *left;
+//		*left = *right;
+//		*right = tmp;
+//		left++;
+//		right--;
+//	}
+//
+//}
+//void left_move(char *arr,int k)
+//{
+//	assert(arr);
+//	int len = strlen(arr);
+//	assert(k < len);
+//	reverse(arr, arr + k - 1);//逆序左边
+//	reverse(arr + k,arr+len-1 );//逆序右边
+//	reverse(arr,arr+len-1);//逆序整体
+//
+//}
+//int main()
+//{
+//	char arr[] = "abcdef";
+//	left_move(arr, 2);
+//	printf("%s\n", arr);
+//	return 0;
+//}
+#include<assert.h>
+void reverse(char* left, char* right)
 {
-	int line = 0;
-	scanf("%d", &line);
-	//打印上半部分
+	assert(left);
+	assert(right);
+	while (left<right)
+	{
+		char tmp = *left;
+		*left = *right;
+		*right = tmp;
+		left++;
+		right--;
+	}
+
+}
+void left_move(char *arr,int k)
+{
+	assert(arr);
+	int len = strlen(arr);
+	assert(k <=len);
+	reverse(arr, arr + k - 1);//逆序左边
+	reverse(arr + k,arr+len-1 );//逆序右边
+	reverse(arr,arr+len-1);//逆序整体
+
+}
+
+int is_left_move(char*s1,char *s2)
+{
+	int len = strlen(s1);
 	int i = 0;
-	for (i = 0; i < line; i++)
+	for (i = 0; i < len; i++)
 	{
-		int j = 0;
-		//打印空格
-		for (j = 0; j < line - 1 - i; j++)
+		left_move(s1, 1);
+		int ret = strcmp(s1, s2);
+		if (ret == 0)
 		{
-			printf(" ");
+			return 1;
 		}
-		//打印*
-		for (j = 0; j < 2*i+1; j++)
-		{
-			printf("*");
-		}
-		printf("\n");
-
-		
 	}
-	//打印下半部分
-	for (i = 0; i < line; i++)
+	return 0;
+}
+int main()
+ {
+	char arr1[] = "abcdef";
+	char arr2[] = "cdefab";
+	int ret = is_left_move(arr1, arr2);
+	if (ret == 1)
 	{
-		//打印空格
-		int j = 0;
-		for (j = 0; j <=i; j++)
-		{
-			printf(" ");
-		}
-		//打印*
-		for (j = 0; j <2*(line-1-i)-1 ; j++)
-		{
-			printf("*");
-		}
-		printf("\n");
+		printf("Yes\n");
 	}
-
-
-
-
- }
+	else
+	{
+		printf("No\n");
+	}
+}
