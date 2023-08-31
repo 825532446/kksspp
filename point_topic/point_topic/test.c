5e9ca6210c2acc6044050b987f344f472f3d197c
hcy <825532446@qq.com>
2023-08-31 13:37:10 +0800

    ‘指针面试题’

--- point_topic/point_topic/test.c
+++ point_topic/point_topic/test.c
@@ -264,58 +264,179 @@
 //	printf("%s\n", arr);
 //	return 0;
 //}
-#include<assert.h>
-void reverse(char* left, char* right)
+
+//#include<assert.h>
+//void reverse(char* left, char* right)
+//{
+//	assert(left);
+//	assert(right);
+//	while (left<right)
+//	{
+//		char tmp = *left;
+//		*left = *right;
+//		*right = tmp;
+//		left++;
+//		right--;
+//	}
+//
+//}
+//void left_move(char *arr,int k)
+//{
+//	assert(arr);
+//	int len = strlen(arr);
+//	assert(k <=len);
+//	reverse(arr, arr + k - 1);//ÄæĞò×ó±ß
+//	reverse(arr + k,arr+len-1 );//ÄæĞòÓÒ±ß
+//	reverse(arr,arr+len-1);//ÄæĞòÕûÌå
+//
+//}
+//
+//int is_left_move(char*s1,char *s2)
+//{
+//	int len = strlen(s1);
+//	int i = 0;
+//	for (i = 0; i < len; i++)
+//	{
+//		left_move(s1, 1);
+//		int ret = strcmp(s1, s2);
+//		if (ret == 0)
+//		{
+//			return 1;
+//		}
+//	}
+//	return 0;
+//}
+//int main()
+// {
+//	char arr1[] = "abcdef";
+//	char arr2[] = "cdefab";
+//	int ret = is_left_move(arr1, arr2);
+//	if (ret == 1)
+//	{
+//		printf("Yes\n");
+//	}
+//	else
+//	{
+//		printf("No\n");
+//	}
+//}
+
+//#include<assert.h>
+//void reverse(char* left, char* right)
+//{
+//	assert(left);
+//	assert(right);
+//	while (left < right)
+//	{
+//		char tmp = *left;
+//		*left = *right;
+//		*right = tmp;
+//		left++;
+//		right--;
+//	}
+//
+//}
+//void left_move(char* arr, int k)
+//{
+//	assert(arr);
+//	int len = strlen(arr);
+//	assert(k <= len);
+//	reverse(arr, arr + k - 1);//ÄæĞò×ó±ß
+//	reverse(arr + k, arr + len - 1);//ÄæĞòÓÒ±ß
+//	reverse(arr, arr + len - 1);//ÄæĞòÕûÌå
+//
+//}
+//
+//int is_left_move(char* str1, char* str2)
+//{
+//	int len1= strlen(str1);
+//	int len2 = strlen(str2);
+//	if (len1 != len2)
+//	{
+//		return 0;
+//	}
+//	//1.ÔÚstr1×Ö·û´®ÖĞ×·¼ÓÒ»¸öÖ¸ÏòµÄ×Ö·û´®µÄ×Ó´®
+//	// strcat ×Ö·û´®×·¼Ó
+//	//strcat(str1, str2);//err
+//	strncat(str1, str2, 6);
+//	//2.ÅĞ¶Ïstr2Ö¸ÏòµÄ×Ö·û´®ÊÇ·ñÊÇstr1Ö¸ÏòµÄ×Ö·û´®µÄ×Ó´®
+//	//ÕÒ×Ó´®strstr
+//	char *ret=strstr(str1, str2);
+//	if (ret == NULL)
+//	{
+//		return 0;
+//	}
+//	else
+//	{
+//		return 1;
+//	}
+//
+//
+//	return 0;
+//}
+//int main()
+//{
+//	char arr1[30] = "abcdef";
+//	char arr2[] = "cdef";
+//	int ret = is_left_move(arr1, arr2);
+//	if (ret == 1)
+//	{
+//		printf("Yes\n");
+//	}
+//	else
+//	{
+//		printf("No\n");
+//	}
+//}
+
+//ÑîÊÏ¾ØÕó
+int FindNum(int arr[3][3],int k,int *px,int *py)
 {
-	assert(left);
-	assert(right);
-	while (left<right)
+	int x = 0;
+	int y = *py - 1;
+	while (x<=*py-1&&y>=0)
 	{
-		char tmp = *left;
-		*left = *right;
-		*right = tmp;
-		left++;
-		right--;
+
+	
+	if (arr[x][y] > k)
+	{
+		y--;
 	}
-
-}
-void left_move(char *arr,int k)
-{
-	assert(arr);
-	int len = strlen(arr);
-	assert(k <=len);
-	reverse(arr, arr + k - 1);//ÄæĞò×ó±ß
-	reverse(arr + k,arr+len-1 );//ÄæĞòÓÒ±ß
-	reverse(arr,arr+len-1);//ÄæĞòÕûÌå
-
-}
-
-int is_left_move(char*s1,char *s2)
-{
-	int len = strlen(s1);
-	int i = 0;
-	for (i = 0; i < len; i++)
+	else if(arr[x][y] <k)
 	{
-		left_move(s1, 1);
-		int ret = strcmp(s1, s2);
-		if (ret == 0)
-		{
-			return 1;
-		}
-	}
-	return 0;
-}
-int main()
- {
-	char arr1[] = "abcdef";
-	char arr2[] = "cdefab";
-	int ret = is_left_move(arr1, arr2);
-	if (ret == 1)
-	{
-		printf("Yes\n");
+		x++;
 	}
 	else
 	{
-		printf("No\n");
+		*px = x;
+		*py = y;
+		
+		return 1;
 	}
+	}
+	*px = -1;
+	*py = -1;
+	return 0;
 }
+
+int main()
+{
+	int x = 3;
+	int y = 3;
+	int arr[3][3] = { {1,2,3},{4,5,6 },{7,8,9 }};
+	int k = 4;
+	//·µ»ØĞÍ²ÎÊı
+	int ret = FindNum(arr, k, &x, &y);
+	if (ret == 1)
+	{
+		printf("ÕÒµ½ÁË\n");
+		printf("ÏÂ±êÊÇ%d %d\n", x, y);
+	}
+	else
+	{
+		printf("ÕÒ²»µ½\n");
+	}
+	return 0;
+}
+
+
